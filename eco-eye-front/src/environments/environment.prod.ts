cf5c4dfc08c0195f05bbfe76d26421a1d63e8d12
Touluse <tal.argamanbib@gmail.com>
2025-07-14 14:18:50 +0300

    build

--- eco-eye-front/src/environments/environment.prod.ts
+++ eco-eye-front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: "https://readhand-moouaplqla-uc.a.run.app/readHand"
+  production: true,
+  apiUrl: "https://us-central1-eco-eye.cloudfunctions.net"
 };
