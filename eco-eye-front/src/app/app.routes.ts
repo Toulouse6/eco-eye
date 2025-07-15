b940f96088f5061b48edc49908b5c5c5b97a2eb6
Touluse <tal.argamanbib@gmail.com>
2025-07-15 15:15:45 +0300

    cleanup

--- eco-eye-front/src/app/app.routes.ts
+++ eco-eye-front/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { EcoReportComponenet } from './eco-report/eco-report.component';
+import { EcoReportComponent } from './eco-report/eco-report.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'eco-report', component: EcoReportComponenet }
+  { path: 'eco-report', component: EcoReportComponent }
 ];
