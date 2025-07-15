13b17f1bb8f05a7fe1ff5a720a8baa6dcd7c7880
Touluse <tal.argamanbib@gmail.com>
2025-07-15 17:35:13 +0300

    reorder

--- eco-eye-front/src/app/home/home.component.ts
+++ eco-eye-front/src/app/home/home.component.ts
@@ -29,7 +29,7 @@ export class HomeComponent implements OnInit {
     ];
 
     filteredModels: string[] = [...this.models];
-    isLoading = true;
+    isLoading = false;
     errorMessage: string | null = null;
 
     private backgroundAudio = new Audio('assets/audio/nova-notes.mp3');
